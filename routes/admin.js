ec6a4acdd25e523bbd12d0ed4c16180b4cebff12
Ananya Pathak <anyalko043@gmail.com>
2024-12-21 18:26:31 +0530

    Added  admin route

---
+++ routes/admin.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const adminRouter = Router();
+
+adminRouter.use
+
+adminRouter.post('/signup', (req, res) => {
+
+})
+
+adminRouter.post('/signin', (req, res) =>{
+
+})
+
+adminRouter.post('/CreateCourses', (req, res)=> {
+
+})
+
+adminRouter.get('/course/bulk', (req, res) => {
+
+})
+
+adminRouter.put('/update', (req, res) => {
+
+})
+
+module.exports = {
+  adminRouter: adminRouter
+}
