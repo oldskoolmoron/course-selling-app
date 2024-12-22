e6262ae959dda4987d81e591d8c3a960621ad9d4
Ananya Pathak <anyalko043@gmail.com>
2024-12-22 20:36:34 +0530

    Confirm server routes and push for consistency

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+require('dotenv').config();
+
 const express = require("express");
 const { createUserRoutes, userRouter } = require('./routes/user');
 const { createCourseRoutes, courseRouter } = require('./routes/course');
 const { adminRouter } = require('./routes/admin');
 const app = express();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const { userModel,adminModel,courseModel,purchaseModel } = require('./db');
 
-const jsonwebtoken = require('jsonwebtoken');
+mongoose.connect(process.env.MONGODB_URI, {})
+.then(()=>{
+  console.log("connected to MongoDB");
+}).catch((error)=>{
+  console.error("MongoDB connection error:", error.message);
+})
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
 app.use("/admin", adminRouter);
 
-
+  
 
 app.listen(3000, ()=>{
   console.log("server is running on port 3000")
