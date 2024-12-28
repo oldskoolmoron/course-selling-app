0c64501ef1da4a61eb2e63403afff5f2bbaea5af
Ananya Pathak <anyalko043@gmail.com>
2024-12-29 03:22:50 +0530

    Added course preview and purchase endpoints with middleware for authentication

--- routes/course.js
+++ routes/course.js
@@ -1,15 +1,30 @@
 const { Router } = require("express");
 const courseRouter = Router();
+const { userMiddleware } = require("../middleware/user");
+const { purchaseModel,courseModel } = require("../db");
+const jwt = require("jsonwebtoken");
 
-courseRouter.get("/purchase", (req, res)=>{
 
+courseRouter.get("/purchase",userMiddleware,async (req, res)=>{
+  const userId = req.userId;
+  const courseId =req.body.courseId;
+  //One thing left is that I also need to add a checkpoint to verify that the user has actually paid the price.
+  await purchaseModel.create({
+    userId,
+    courseId
+  })
+  res.json({
+    message:"Your have successfully bought the course"
+  })
 })
 
-courseRouter.get("/preview", (req, res)=> {
-  //in the real world you would expect money interference
-
-
+courseRouter.get("/preview",async(req, res) =>{
+  const courses= await courseModel.find({});
+  res.json({
+    courses
+  })
 })
+
 
 module.exports = {
   courseRouter: courseRouter
